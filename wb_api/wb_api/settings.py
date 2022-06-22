20132a4113a3881691b7e2188fd496d6a4cab387
denis <pekshev.ds@gmail.com>
2022-06-22 14:09:37 +0300

    20220666_001 configparser for secret

--- wb_api/wb_api/settings.py
+++ wb_api/wb_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from configparser import ConfigParser
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ifu$1v-u-gei&ob^ma3sli#j9=10fvycekxa_e!mbspc1c61t)'
+cp = ConfigParser()
+cp.read('config.ini')
+
+SECRET_KEY = cp.get('DEFAULT', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
